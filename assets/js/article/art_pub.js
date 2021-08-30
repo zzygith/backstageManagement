079c5a04d3404c89de30cac632ad877ea35963b2
Zhongyu Zhang <zhangzhongyu98@gmail.com>
2021-08-30 13:11:30 -0600

    Optimize art_pub

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -3,28 +3,10 @@ $(function () {
     var form = layui.form
     //Initialize rich editor
     /* initEditor() */
-/*     tinymce.init({
+    tinymce.init({
     selector: 'textarea#default'
-    }); */
-/*     tinymce.init({
-        selector: 'textarea#default',
+    });
 
-        setup: function (editor) {
-          editor.on('init', function () {
-            editor.setContent('Using the on init stuff!');
-          });
-        }
-      }); */
-
-/*     if (sessionStorage.getItem('article') !== null) {
-        console.log(JSON.parse(sessionStorage.getItem('article')))
-        let artContent = JSON.parse(sessionStorage.getItem('article'));
-
-        $('[name=title]').val(artContent.title);
-        $('[name=cate_id]').val(artContent.cate_id);
-        $('[placeholder="Please select article catagory"]').val($('[name=cate_id] option:selected').text())
-        $('[name=content]').val(artContent.content);
-    } */
 
 //If edition is clicked, then execute this function. Get the edited article and show it in form.
     if (sessionStorage.getItem('editModel')) {
@@ -35,54 +17,37 @@ $(function () {
                 success: function (res) {
                     if (res.status !== 0) {
                         return layer.msg('Failed to delete article')
-                }
+                    }
                     layer.msg('Load article successfully')
-                let artContent = res.data[0];
+                    let artContent = res.data[0];
                     $('[name=title]').val(artContent.title);
                     $('[name=cate_id]').val(artContent.cate_id);
-                $('[placeholder="Please select article catagory"]').val($('[name=cate_id] option:selected').text());
-                tinymce.init({
-                    selector: 'textarea#default',
-                    setup: function (editor) {
-                        editor.on('init', function () {
-                            editor.setContent(artContent.content);
-                      });
-                    }
-/*                     setup: function (editor) {
-                        editor.on('init', areaT);
-                    } */
-                });
-/*                 function areaT() {
-                    tinymce.get("default").setContent(artContent.content); 
-                } */
-                sessionStorage.removeItem('editModel');
-                editArticle();
+                    $('[placeholder="Please select article catagory"]').val($('[name=cate_id] option:selected').text());
+                    tinymce.get("default").setContent(artContent.content);
+/*                  tinymce.init({
+                        selector: 'textarea#default',
+                        setup: function (editor) {
+                            editor.on('init', function () {
+                                editor.setContent(artContent.content);
+                            });
+                        }
+                    }); */
+
+                    sessionStorage.removeItem('editModel');
+
                 }
             })
-    }else {
+    }
+    
+/*     else {
         tinymce.init({
             selector: 'textarea#default'
         });
-        editArticle();
-    }
 
-/*             $.ajax({
-                method: 'GET',
-                url: '/my/article/' + edi_id,
-                success: function (res) {
-                    if (res.status !== 0) {
-                        return layer.msg('Failed to delete article')
-                    }
-                    layer.msg('Load article successfully')
-                    // sessionStorage.setItem('article', res.data);
-                    sessionStorage.setItem('article', JSON.stringify(res.data));
-                    //Skip to publish article page
-                    window.parent.$('.pubArt').click();
-                }
-            }) */
+    } */
 
 
-    function editArticle() {
+
     let art_state = 'submitted';
     //If click 'save as draft', then change the state
     $('#btnSave2').on('click', function () {
@@ -107,47 +72,9 @@ $(function () {
                 }
                 layer.msg('Publish articles successfully');
                 window.parent.$('.artList').click();
-
             }
-
         })
-
-
     })
-}
-/*     let art_state = 'submitted';
-    //if click 'save as draft', then change the state
-    $('#btnSave2').on('click', function () {
-        art_state = 'draft';
-    })
-
-    $('#form_pub').on('submit', function (e) {
-        e.preventDefault();
-        console.log($(this).serialize());
-        let fd = new FormData($(this)[0]);
-        fd.append('state', art_state);
-        var wwe = new Blob([''], { type: "image/png" });
-        //fd.append('cover_img', wwe); 
-        $.ajax({
-            method: 'POST',
-            url: '/my/article/add',
-            data: fd,
-            contentType: false,
-            processData: false,
-            success: function (res) {
-                console.log(res);
-                if (res.status !== 0) {
-                    return layer.msg('Failed to publish article')
-                }
-                layer.msg('Publish articles successfully');
-                window.parent.$('.artList').click();
-
-            }
-
-        })
-
-
-    }) */
 
 
 })
